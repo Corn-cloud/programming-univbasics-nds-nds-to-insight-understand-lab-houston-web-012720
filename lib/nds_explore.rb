fbe3e442a05d7a2ff73595245471127271760898
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-10 06:50:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,6 +16,6 @@ def print_first_directors_movie_titles
   spilberg = directors_database [0][:movies]
   while row_index < spilberg.length do
     titles = spilberg[row_index][:title]
-    
+    put titles 
   
 end
